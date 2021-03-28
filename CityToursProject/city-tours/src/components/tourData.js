e5fdaa20c25fb4d61d888a4d35f74a0e8ff9f730
María Fernanda Galárraga Molina <43796434+FernandaGalarraga@users.noreply.github.com>
2021-03-28 17:14:32 -0500

    citytours

---
+++ CityToursProject/city-tours/src/components/tourData.js
@@ -0,0 +1,68 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "nueva york",
+    img: "./img/newyork.jpeg",
+    name: "Puente de nueva york tour",
+    info:
+      "El Brooklyn Bridge es más que un puente que cruza el East River, es todo un símbolo de la ciudad de Nueva York que además marcó un hito en la historia al utilizar por primera vez en este tipo de construcciones el acero y por ser durante 20 años el puente colgante más largo del mundo."
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "ciudad de las luces tour",
+    info:
+      "Famosa por sus historias de amor, revoluciones e intrigas reales, París y sus emblemáticos monumentos se erigen como recordatorios visibles de algunos de los períodos más románticos de la historia."
+  },
+  {
+    id: 3,
+    city: "londres",
+    img: "./img/london.jpeg",
+    name: "Palacio de Buckingham tour",
+    info:
+      "Banqueting House es la única parte que queda del Palacio de Whitehall debido a un incendio. Lo que solía ser el palacio real europeo más grande y se originó en el siglo XIV. Todavía se utiliza hoy como lugar para eventos especiales de Royal."
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo tour",
+    info:
+      "Tokio es el epítome de la metrópolis moderna: trenes bala se deslizan por los tejados de la ciudad y pasan por delante de nuevos y relucientes rascacielos; inmensas pantallas de televisión que se ciernen sobre las intersecciones proyectan sus mensajes sobre la multitud."
+  },
+  {
+    id: 5,
+    city: "seul",
+    img: "./img/seul.jpg",
+    name: "Rio Han Tour",
+    info:
+      "A lo largo del río hay 12 parques con instalaciones deportivas que harán las delicias de toda la familia, una buena escapada para relajarse del día a día de la ciudad. Esta cruzado por 26 puentes, siendo el más famoso e interesante el Puente Banpodaegyo, con su fuente de arco iris."
+  }
+  ,
+  {
+    id: 6,
+    city: "Roma",
+    img: "./img/roma.jpg",
+    name: "Coliseo romano tour",
+    info:
+      "Es un anfiteatro de la época del Imperio romano, construido en el siglo I. Está ubicado en el este del Foro Romano, y fue el más grande de los que se construyeron en el Imperio romano."
+  },
+  {
+    id: 7,
+    city: "Honolulu",
+    img: "./img/honolulu.jpg",
+    name: "Hawai travel tour",
+    info:
+      " Honolulu es una ciudad muy activa y ofrece un montón de cosas divertidas que hacer: las increíbles playas de la costa, lugares de interés histórico, monumentos, tiendas y centros comerciales, entre otras cosas."
+  }
+  ,
+  {
+    id: 8,
+    city: "Latacunga",
+    img: "./img/cotopaxi.jpg",
+    name: "cotopaxi summit tour",
+    info:
+      "El volcán Cotopaxi, un cono nevado casi perfecto que se yergue a 5.897 metros de altitud es algo único en el planeta y es quizá, junto a Galápagos, el mayor símbolo de nuestra geografía natural reconocida en el mundo entero."
+  }
+];
